470545047eb89e2abce95f96783325094e1f7cdd
rarean <db.robot@yaoo.com>
2020-06-09 20:26:31 -0500

    fix scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 window.onload = choosePic;
 function choosePic() {
-  img = document.querySelectorAll(".rounded-circle");
+  var img = document.querySelectorAll(".rounded-circle");
     for(i=0; i < img.length; i++){
       const myPix = new Array("imgs/letter_A.png", "imgs/Letter_b.png", "imgs/letter_c.png", "imgs/letter_d.png", "imgs/letter_e.png", "imgs/letter_f.png", "imgs/letter_g.png", "imgs/letter_h.png", "imgs/letter_i.png", "imgs/letter_j.png", "imgs/letter_k.png", "imgs/letter_l.png", "imgs/letter_m.png", "imgs/letter_n.png");
-      let randomNum = Math.floor(Math.random() * myPix.length);
+      var randomNum = Math.floor(Math.random() * myPix.length);
       img[i].src = myPix[randomNum];
     }
+ }
 
 $(function(){
   $("#nav-placeholder").load("nav.html");
 });
+
