cd3de6053f9108c2cab8733c3bb72b09629e4440
Rachel Brown <rachelcbrown13@gmail.com>
2020-06-10 17:05:12 -0500

    img changed for all images

--- scripts.js
+++ scripts.js
@@ -2,11 +2,16 @@
 $(function(){
     function choosePic() {
       var img = document.querySelectorAll(".rounded-circle");
-        for(i=0; i < img.length; i++){
-          const myPix = new Array("imgs/letter_A.png", "imgs/Letter_b.png", "imgs/letter_c.png", "imgs/letter_d.png", "imgs/letter_e.png", "imgs/letter_f.png", "imgs/letter_g.png", "imgs/letter_h.png", "imgs/letter_i.png", "imgs/letter_j.png", "imgs/letter_k.png", "imgs/letter_l.png", "imgs/letter_m.png", "imgs/letter_n.png");
+         for(i=0; i < img.length; i++){
+          const myPix = new Array("img/letter_A.png", "img/Letter_b.png", "img/letter_c.png", "img/letter_d.png", "img/letter_e.png", "img/letter_f.png", "img/letter_g.png", "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", "img/letter_l.png", "img/letter_m.png", "img/letter_n.png");
           var randomNum = Math.floor(Math.random() * myPix.length);
           img[i].src = myPix[randomNum];
         }
       }
+  function total(){
+     let quantity = 0;
+     $('bookNum').val = el ;
+     console.log ('this')
+  }
 });
 
