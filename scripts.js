16edef8026914ff9fbc6ee4760d74aae306dbe6f
Rachel Brown <rachelcbrown13@gmail.com>
2020-06-12 22:03:48 -0500

    colorpage added to master

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
-
 $(function(){
-    function choosePic() {
-      var img = document.querySelectorAll(".rounded-circle");
-         for(i=0; i < img.length; i++){
-          const myPix = new Array("img/letter_A.png", "img/Letter_b.png", "img/letter_c.png", "img/letter_d.png", "img/letter_e.png", "img/letter_f.png", "img/letter_g.png", "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", "img/letter_l.png", "img/letter_m.png", "img/letter_n.png");
-          var randomNum = Math.floor(Math.random() * myPix.length);
-          img[i].src = myPix[randomNum];
-        }
+  window.onload = choosePic;
+  function choosePic() {
+    var img = document.querySelectorAll(".circle");
+       for (i=0; i < img.length; i++){
+        const myPix = new Array("img/letter_A.png", "img/Letter_b.png", "img/letter_c.png", "img/letter_d.png", "img/letter_e.png", "img/letter_f.png", "img/letter_g.png", "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", "img/letter_l.png", "img/letter_m.png", "img/letter_n.png", "img/letter_o.png", "img/letter_percy.png", "img/letter_q.png", "img/letter_r.png", "img/letter_s.png",);
+        var randomNum = Math.floor(Math.random() * myPix.length);
+        img[i].src = myPix[randomNum];
       }
+  }
+  function fadeIn(){
+    $(".cover").fadeIn("10000");
+  }
+
   function total(){
      let quantity = 0;
      $('bookNum').val = el ;
-     console.log ('this')
+     console.log ('this');
   }
 });
 
