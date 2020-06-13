314592cabe12d9e844c7e190396134c518e53ae0
rarean <db.robot@yaoo.com>
2020-06-12 22:18:22 -0500

    fixes for uglify

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(function(){
   function choosePic() {
     var img = document.querySelectorAll(".circle");
        for (i=0; i < img.length; i++){
-        const myPix = new Array("img/letter_A.png", "img/Letter_b.png", "img/letter_c.png", "img/letter_d.png", "img/letter_e.png", "img/letter_f.png", "img/letter_g.png", "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", "img/letter_l.png", "img/letter_m.png", "img/letter_n.png", "img/letter_o.png", "img/letter_percy.png", "img/letter_q.png", "img/letter_r.png", "img/letter_s.png",);
+        const myPix = new Array("img/letter_A.png", "img/Letter_b.png", "img/letter_c.png", "img/letter_d.png", "img/letter_e.png", "img/letter_f.png", "img/letter_g.png", "img/letter_h.png", "img/letter_i.png", "img/letter_j.png", "img/letter_k.png", "img/letter_l.png", "img/letter_m.png", "img/letter_n.png", "img/letter_o.png", "img/letter_percy.png", "img/letter_q.png", "img/letter_r.png", "img/letter_s.png");
         var randomNum = Math.floor(Math.random() * myPix.length);
         img[i].src = myPix[randomNum];
       }
@@ -13,9 +13,9 @@ $(function(){
   }
 
   function total(){
-     let quantity = 0;
-     $('bookNum').val = el ;
-     console.log ('this');
+     var quantity = 0;
+     $("bookNum").val = el ;
+     console.log ("this");
   }
 });
 
